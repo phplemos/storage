385fa998437747ab6633351df1a5ffa23ef995f1
phplemos <phpinheirolemos@outlook.com>
2023-07-24 15:47:56 -0300

    deleted:    todo-list/app/src/main/java/com/todolist/app/controller/CommentController.java 	deleted:    todo-list/app/src/main/java/com/todolist/app/model/Category.java 	deleted:    todo-list/app/src/main/java/com/todolist/app/model/Comment.java 	deleted:    todo-list/app/src/main/java/com/todolist/app/repository/CategoryRepository.java 	deleted:    todo-list/app/src/main/java/com/todolist/app/repository/CommentRepository.java 	deleted:    todo-list/app/src/main/java/com/todolist/app/service/CategoryService.java 	deleted:    todo-list/app/src/main/java/com/todolist/app/service/CommentService.java
    	modified:   todo-list/app/src/main/java/com/todolist/app/model/Task.java
    	modified:   todo-list/app/src/main/java/com/todolist/app/repository/TaskRepository.java
    	modified:   todo-list/app/src/main/java/com/todolist/app/service/TaskService.java

--- todo-list/app/src/main/java/com/todolist/app/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.todolist.app.repository;
-
-import com.todolist.app.model.Category;
-import com.todolist.app.model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment,Integer> {
-
-}
